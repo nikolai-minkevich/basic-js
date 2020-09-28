5bbd7122854f6da623ef0977ac06293c11d83aa0
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2020-09-28 15:33:03 +0300

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
+let chain = []
+
+function formatChain(chain) {
+  let s = ''
+  for (let i = 0; i < chain.length; i++) {
+    if (chain[i] !== ' ') chain[i] += ' '
+    s += '( ' + chain[i] + ')'
+    if (chain[i + 1] !== undefined)
+      s += '~~'
+  }
+  return s
+}
+
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return chain.length
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value==null) value='null'
+      if (value==0) value=='0'
+    chain.push(value.toString())
+
+    return this
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (chain[position - 1] == undefined) {
+      chain = []
+      throw Error
+    }
+    if (typeof parseFloat(position) !== 'number') {
+      chain = []
+      throw Error
+    }
+    chain.splice(position - 1, 1)
+    return this
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    chain.reverse()
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    output=formatChain(chain) 
+    chain=[]
+
+    return output
   }
 };
 
