80b493a057565d10beec8aaaafb42e5bcf31c0fd
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2020-09-30 15:46:07 +0300

    refactor: improve calculateDepth() function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -4,27 +4,16 @@ module.exports = class DepthCalculator {
 
 
   calculateDepth(arr) {
-
-    if (!Array.isArray(arr)) {
-      return 1
-    }
-
     let depth = 1
     let maxdepth = 1
-
     arr.forEach(element => {
       if (Array.isArray(element)) {
         depth = 1
         depth += this.calculateDepth(element)
         if (depth > maxdepth) {
           maxdepth = depth
-          depth = 0
         }
-        return depth
-      } else {
-
-        return depth
-      }
+      } 
     });
 
     return maxdepth
